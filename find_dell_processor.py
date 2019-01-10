cad021c15f32e0c0d55705e4002a25fff472c71e
Nildo Adão <nildoadao@yahoo.com.br>
2019-01-10 11:00:09 -0200

    Commit inicial

---
+++ find_dell_processor.py
@@ -0,0 +1,60 @@
+# Script para localizar o modelo do processor de servidores Dell
+# Consulta a partir do site dell.com/support
+# Criado por: Nildo Adao 07/01/2019
+
+import requests, sys, argparse, BeautifulSoup, json
+
+parser = argparse.ArgumentParser(description="Script em Python para obter o processor de servidores Dell.")
+
+parser.add_argument("-tag",help="Service Tag", required=False)
+parser.add_argument("-list",help="Lista de tags", required=False)
+
+args=vars(parser.parse_args())
+
+def find_processor(tag):
+
+    # Link para suporte da Dell
+    url = "https://www.dell.com/support/home/pt/br/04/product-support/servicetag/{}/configuration".format(tag)
+
+    try:
+        response = requests.get(url)
+        results = []
+        soap = BeautifulSoup.BeautifulSoup(response.text)
+        hdn_parts = json.loads(soap.find(id="hdnParts").get("value"))
+
+        for item in hdn_parts:
+            for part in item[u"Parts"]:
+                if "processor" in part[u"Description"].lower():
+                    results.append(item[u"SkuDescription"])  
+        
+        print("Service Tag: {}, Processador: {}, Quantidade: {}".format(tag, results[0], len(results)))
+
+    except (Exception) as e:
+
+        if(type(e) is requests.exceptions.ConnectionError):
+            print("Falha na comunicacao com dell.com/support, verifique a conexao e tente novamente")
+
+        else:
+            print("Service Tag: {}, falha ao obter processador".format(tag))
+        return
+
+    
+
+if __name__ == "__main__":
+    try:
+        if (args["tag"]):
+            tag = args["tag"]
+            find_processor(tag)
+
+        elif (args["list"]):
+            tags = args["list"].split(",")
+            for item in tags:
+                find_processor(item)
+        else:
+            print("Erro, deve ser fornecido algum parametro para o script\n Ex: find_dell_processor -tag ABCDGG")
+    except:
+        print("Erro, deve ser fornecido algum parametro para o script\n Ex: find_dell_processor -tag ABCDGG")
+
+    sys.exit()
+
+    
